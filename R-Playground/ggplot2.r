01ae2c1cfd0012282a1951de0f13e72152e730ce
Dulina-Perera <dulina.perera@outlook.com>
2024-05-19 20:50:31 +0530

    R-Playground: Learnt about aesthetic mappings.

--- R-Playground/ggplot2.r
+++ R-Playground/ggplot2.r
@@ -1,6 +1,8 @@
 library(tidyverse)
 
-mpg
+# Load the mpg dataset.
+data(mpg, package = "ggplot2")
+print(mpg)
 
 ggplot2::ggplot(data = mpg) +
-  ggplot2::geom_point(mapping = ggplot2::aes(x = displ, y = hwy, color = class))
+  ggplot2::geom_point(mapping = ggplot2::aes(x = displ, y = hwy, colour = class))
